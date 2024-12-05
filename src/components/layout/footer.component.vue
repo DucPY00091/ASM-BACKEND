2c4cafa02693493a32dc9075d303fbe8fb2000a8
Duc <ducntpy00091@gmail.com>
2024-12-05 21:54:37 +0700

    first commit

---
+++ src/components/layout/footer.component.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Nguyễn Thanh Đức - PY00091</p>
+    </div>
+  </footer>
+</template>
+<style></style>
